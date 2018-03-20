39aa48623f5a3cb3a1c7fa5fc1c65c85b4de4d58
haidang <giant9x@gmail.com>
2018-03-21 00:38:50 +0700

    first upload

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix'=>'/'], function() {
+    Route::get('/', ['as'=> 'getHome', 'uses'=>'HomeController@getHome']);
+    Route::get('/get/{query}', ['as'=>'getList', 'uses'=>'HomeController@getList']);
+});
